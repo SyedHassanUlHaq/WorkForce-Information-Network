1a0ed4e7477b33c880e6f508966fa32de22dd26f
SyedHassanUlHaq <21b-029-cs@students.uit.edu>
2023-07-07 00:52:11 +0500

    updated code

--- login.py
+++ login.py
@@ -14,6 +14,7 @@ class Login_System:
         self.root.title(" WIN Login System | Developed By Hassan")
         self.root.geometry("1350x700+0+0")
         self.root.config(bg = '#fafafa')
+
         # Images
         self.phone_image = ImageTk.PhotoImage(file = "images/phone.png")
         self.lbl_Phone_image = Label(self.root, image = self.phone_image, bd = 0).place(x = 200, y = 50)
@@ -38,7 +39,7 @@ class Login_System:
         hr = Label(login_frame, bg = "lightgray").place(x = 50, y = 370, width=250, height=2)
         or_ = Label(login_frame, text = "OR", bg = "white", font = ("times new roman", 15, "bold")).place(x = 150, y = 355)
 
-        btn_forget = Button(login_frame, text = "Forget Password?", font = ("times new roman", 13), bg = "white", fg = "#00759E", bd = 0, activebackground="white", activeforeground="#00759E").place(x=100, y=390)
+        btn_forget = Button(login_frame, command = self.forget_window, text = "Forget Password?", font = ("times new roman", 13), bg = "white", fg = "#00759E", bd = 0, activebackground="white", activeforeground="#00759E", cursor = "hand2").place(x=100, y=390)
         
         # Frame 2
         # register_frame = Frame(self.root, bd = 2, relief=RIDGE, bg = "white")
@@ -49,20 +50,20 @@ class Login_System:
 
         # Animation images
         self.im1 = ImageTk.PhotoImage(file = "images/im1.png")
-        self.im1 = ImageTk.PhotoImage(file = "images/im2.png")
-        self.im1 = ImageTk.PhotoImage(file = "images/im3.png")
+        self.im2 = ImageTk.PhotoImage(file = "images/im2.png")
+        self.im3 = ImageTk.PhotoImage(file = "images/im3.png")
 
         self.lbl_change_image = Label(self.root, bg = "white")
-        self.lbl_change_image.place(x = 40, y = 153, width = 240, height = 428)
+        self.lbl_change_image.place(x = 367, y = 153, width = 240, height = 428)
 
         self.animate()
 
     def animate(self):
         self.im = self.im1
-        # self.im1 = self.im2
-        # self.im2 = self.im3
-        # self.im3 = self.im
-        # self.lbl_change_image.config(image = self.im)
+        self.im1 = self.im2
+        self.im2 = self.im3
+        self.im3 = self.im
+        self.lbl_change_image.config(image = self.im)
         self.lbl_change_image.after(2000, self.animate)
 
 
@@ -91,8 +92,45 @@ class Login_System:
             messagebox.showerror("Error", f"Error due to : {str(ex)}", parent = self.root)
 
 
+    def forget_window(self):
+        con = sqlite3.connect(database=r'win.db')
+        cur = con.cursor()
+        try:
+            if self.employee_id.get() == "":
+                messagebox.showerror('Error', "Employee ID must be required", parent = self.root)
+            else:
+                cur.execute("select email from employee where eid = ?", (self.employee_id.get(),))
+                email = cur.fetchone()
+                if email == None:
+                    messagebox.showerror('Error', "Invalid Employee ID, try again", parent = self.root)
+                else:
+                    self.var_otp = StringVar()
+                    self.var_new_pass = StringVar()
+                    self.var_conf_pass = StringVar()
+                    # call send_email_function
+                    self.forget_win = Toplevel(self.root)
+                    self.forget_win.title('RESET PASSWORD')
+                    self.forget_win.geometry('400x350+500+100')
+                    self.forget_win.focus_force()
 
+                    title = Label(self.forget_win, text='Reset Password', font = ('goudy old style', 15, 'bold'), bg = "#3f51b5", fg = "white").pack(side = TOP, fill = X)
+                    lbl_reset = Label(self.forget_win, text = "Enter OTP sent on Registered Email", font = ("times new roman", 15)).place(x = 20, y = 60)
+                    txt_reset = Entry(self.forget_win, textvariable= self.var_otp, font = ("times new roman", 15), bg = 'lightyellow').place(x = 20, y = 100, width = 250, height = 30)
 
+                    self.btn_reset = Button(self.forget_win, text = "SUBMIT", font = ("times new roman", 15),bg = 'lightblue')
+                    self.btn_reset.place(x = 280, y = 100, width = 100, height = 30)
+                                        
+                    new_new_pass = Label(self.forget_win, text = "New Password", font = ("times new roman", 15)).place(x = 20, y = 160)
+                    txt_new_pass = Entry(self.forget_win, textvariable= self.var_new_pass, font = ("times new roman", 15), bg = 'lightyellow').place(x = 20, y = 190, width = 250, height = 30)
+
+                    c_reset = Label(self.forget_win, text = "Confirm Password", font = ("times new roman", 15)).place(x = 20, y = 225)
+                    txt_c_pass = Entry(self.forget_win, textvariable= self.var_conf_pass, font = ("times new roman", 15), bg = 'lightyellow').place(x = 20, y = 255, width = 250, height = 30)
+
+                    self.btn_update = Button(self.forget_win, text = "Update", font = ("times new roman", 15),bg = 'lightblue')
+                    self.btn_update.place(x = 150, y = 300, width = 100, height = 30)
+                       
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error due to : {str(ex)}", parent = self.root)
 
 
 root = Tk()
